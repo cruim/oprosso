8d0dae1a38509f851ec1a9c03daed808626ddd5b
cruim <hedgehog147@gmail.com>
2020-01-24 22:09:31 +0300

    decorator

--- decorator.py
+++ decorator.py
@@ -1,7 +1,15 @@
-def func_count(func):
-    func._iteration_count = 0
-    def wrapper():
-        func._iteration_count += 1
-        print(f'Фукция {func.__name__} была вызвана {func._iteration_count} раз(а)')
-        return func
-    return wrapper
+from functools import wraps
+
+
+def repeat(limit=5):
+    def _repeat(func):
+        func.count = 0
+        @wraps(func)
+        def inner():
+            func.count += 1
+            if func.count > limit:
+                raise Exception('Превышен лимит вызова функции!')
+            print(f'Функция {func.__name__} вызвана {func.count} раз(а)')
+            return func()
+        return inner
+    return _repeat
